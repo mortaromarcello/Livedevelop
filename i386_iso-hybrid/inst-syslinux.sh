87ef81d5ee3a93b58f6790ddf09024543a91ab0e
mortaromarcello <mortaromarcello@gmail.com>
2013-08-25 19:57:12 +0200

    Create inst-syslinux.sh

---
+++ i386_iso-hybrid/inst-syslinux.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#
+# per distro debian. Il pacchetto syslinux deve essere già installato nel sistema.
+#
+
+if [ $UID != 0 ]; then
+	echo "Devi essere root per eseguire questo script."
+	exit
+fi
+
+if [ -z $1 ] & [ -z $2 ]; then
+	echo -e "Uso: ${0} /dev/sd(x) /path/to/mount/\n\n \
+/dev/sd(x): disco dove installare syslinux;\n \
+/path/to/mount/: directory dove verrà montata la prima partizione fat32;\n \
+Attenzione: il disco deve contenere la prima partizione come fat32!"
+	exit
+fi
+mount ${1}1 $2 </dev/null
+if [ -z ${?} ]; then
+	echo "Errore montando ${1}1 in ${2}"
+	exit
+fi
+if [ -e /usr/bin/syslinux ]; then
+	if [ ! -d ${2}/boot/syslinux ]; then
+		echo "Creo la directory ${1}1/boot/syslinux (premere Invio o Crtl-c per uscire)"
+		read
+		mkdir -p ${2}/boot/syslinux
+	fi
+	echo "Copio mbr in ${1} (premere Invio o Crtl-c per uscire)"
+	read
+	cat /usr/lib/syslinux/mbr.bin > $1
+	echo "Installo syslinux in ${1}1 (premere Invio o Crtl-c per uscire)"
+	read
+	syslinux --directory /boot/syslinux/ --install ${1}1
+	umount $2
+	echo "Fatto!"
+fi
