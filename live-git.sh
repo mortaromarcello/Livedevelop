2ea9e5ba060390f0628fcea5c3318b079ec5b6ce
mortaromarcello <mortaromarcello@gmail.com>
2014-01-14 14:46:30 +0100

    nuovo file:   amd64_iso-hybrid/config/packages.chroot/live-boot-doc_4.0~alpha18-1_all.deb 	nuovo file:   amd64_iso-hybrid/config/packages.chroot/live-boot-initramfs-tools_4.0~alpha18-1_all.deb 	nuovo file:   amd64_iso-hybrid/config/packages.chroot/live-boot_4.0~alpha18-1_all.deb 	nuovo file:   amd64_iso-hybrid/config/packages.chroot/live-config-doc_4.0~alpha30-1_all.deb 	nuovo file:   amd64_iso-hybrid/config/packages.chroot/live-config-sysvinit_4.0~alpha30-1_all.deb 	nuovo file:   amd64_iso-hybrid/config/packages.chroot/live-config_4.0~alpha30-1_all.deb 	nuovo file:   i386_iso-hybrid-backports/config/packages.chroot/live-boot-doc_4.0~alpha18-1_all.deb 	nuovo file:   i386_iso-hybrid-backports/config/packages.chroot/live-boot-initramfs-tools_4.0~alpha18-1_all.deb 	nuovo file:   i386_iso-hybrid-backports/config/packages.chroot/live-boot_4.0~alpha18-1_all.deb 	nuovo file:   i386_iso-hybrid-backports/config/packages.chroot/live-config-doc_4.0~alpha30-1_all.deb 	nuovo file:   i386_iso-hybrid-backports/config/packages.chroot/live-config-sysvinit_4.0~alpha30-1_all.deb 	nuovo file:   i386_iso-hybrid-backports/config/packages.chroot/live-config_4.0~alpha30-1_all.deb 	nuovo file:   i386_iso-hybrid/config/packages.chroot/live-boot-doc_4.0~alpha18-1_all.deb 	nuovo file:   i386_iso-hybrid/config/packages.chroot/live-boot-initramfs-tools_4.0~alpha18-1_all.deb 	nuovo file:   i386_iso-hybrid/config/packages.chroot/live-boot_4.0~alpha18-1_all.deb 	nuovo file:   i386_iso-hybrid/config/packages.chroot/live-config-doc_4.0~alpha30-1_all.deb 	nuovo file:   i386_iso-hybrid/config/packages.chroot/live-config-sysvinit_4.0~alpha30-1_all.deb 	nuovo file:   i386_iso-hybrid/config/packages.chroot/live-config_4.0~alpha30-1_all.deb 	nuovo file:   live-git.sh

---
+++ live-git.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+[[ ! -d /tmp/live ]] && mkdir -p /tmp/live; cd /tmp/live
+LIVEDEVELOP="/mnt/sda7/livedevelop/wheezy/i386_iso-hybrid"
+LIVEDEVELOP_GIT="/mnt/sda7/livedevelop-git"
+git clone git://live.debian.net/git/live-build.git
+git clone git://live.debian.net/git/live-boot.git
+git clone git://live.debian.net/git/live-config.git
+#git clone git://live-systems.org/git/live-manual.git
+cd live-build && dpkg-buildpackage -b -uc -us && cd ..
+cd live-boot && dpkg-buildpackage -b -uc -us && cd ..
+cd live-config && dpkg-buildpackage -b -uc -us && cd ..
+#cd live-manual && dpkg-buildpackage -b -uc -us && cd ..
+dpkg -i live-build_*.deb
+dpkg -i live-boot{_,-initramfs-tools,-doc}*.deb
+dpkg -i live-config{_,-sysvinit,-doc}*.deb
+
+cp live-boot{_,-initramfs-tools,-doc}*.deb  ${LIVEDEVELOP}/config/packages.chroot/
+cp live-config{_,-sysvinit,-doc}*.deb  ${LIVEDEVELOP}/config/packages.chroot/
+cp live-boot{_,-initramfs-tools,-doc}*.deb  ${LIVEDEVELOP_GIT}/i386_iso-hybrid/config/packages.chroot/
+cp live-config{_,-sysvinit,-doc}*.deb  ${LIVEDEVELOP_GIT}/i386_iso-hybrid/config/packages.chroot/
+cp live-boot{_,-initramfs-tools,-doc}*.deb  ${LIVEDEVELOP_GIT}/i386_iso-hybrid-backports/config/packages.chroot/
+cp live-config{_,-sysvinit,-doc}*.deb  ${LIVEDEVELOP_GIT}/i386_iso-hybrid-backports/config/packages.chroot/
+cp live-boot{_,-initramfs-tools,-doc}*.deb  ${LIVEDEVELOP_GIT}/amd64_iso-hybrid/config/packages.chroot/
+cp live-config{_,-sysvinit,-doc}*.deb  ${LIVEDEVELOP_GIT}/amd64_iso-hybrid/config/packages.chroot/
+rm -R -f -v /tmp/live
