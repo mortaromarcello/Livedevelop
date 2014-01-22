d7332dc1f3ad5c8b51f7674a8ee099c8d3c307dd
mortaromarcello <mortaromarcello@gmail.com>
2014-01-22 08:01:58 +0100

    modificato:   live-git.sh

--- live-git.sh
+++ live-git.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ### ha bisogno di python 3.3 (installarlo da debian testing) ###
 [[ ! -d /tmp/live ]] && mkdir -p /tmp/live; cd /tmp/live
-LIVEDEVELOP="/mnt/sda7/livedevelop/wheezy/i386_iso-hybrid"
-LIVEDEVELOP_GIT="/mnt/sda7/livedevelop-git"
+LIVEDEVELOP="/mnt/sda7/livedevelop/testing/i386_iso-hybrid"
+LIVEDEVELOP_GIT="/mnt/sda7/livedevelop-git/testing"
 git clone git://live.debian.net/git/live-build.git
 git clone git://live.debian.net/git/live-boot.git
 git clone git://live.debian.net/git/live-config.git
@@ -17,28 +17,19 @@ dpkg -i live-build_*.deb
 dpkg -i live-boot{_,-initramfs-tools,-doc}*.deb
 dpkg -i live-config{_,-sysvinit,-doc}*.deb
 
-#rm -f ${LIVEDEVELOP}/config/packages.chroot/live-build_*.deb
+rm -f ${LIVEDEVELOP}/config/packages.chroot/live-build_*.deb
 rm -f ${LIVEDEVELOP}/config/packages.chroot/live-{boot,config,tools}*.deb
-#rm -f ${LIVEDEVELOP_GIT}/config/packages.chroot/live-build_*.deb
-rm -f ${LIVEDEVELOP_GIT}/config/packages.chroot/live-{boot,config,tools}*.deb
+rm -f ${LIVEDEVELOP_GIT}/testing/i386_iso-hybrid/config/packages.chroot/live-build_*.deb
+rm -f ${LIVEDEVELOP_GIT}/testing/i386_iso-hybrid/config/packages.chroot/live-{boot,config,tools}*.deb
 
-#cp live-build_*.deb ${LIVEDEVELOP}/config/packages.chroot/
+cp live-build_*.deb ${LIVEDEVELOP}/config/packages.chroot/
 cp live-tools_*.deb ${LIVEDEVELOP}/config/packages.chroot/
 cp live-boot{_,-initramfs-tools,-doc}*.deb ${LIVEDEVELOP}/config/packages.chroot/
 cp live-config{_,-sysvinit,-doc}*.deb  ${LIVEDEVELOP}/config/packages.chroot/
 
-#cp live-build_*.deb ${LIVEDEVELOP_GIT}/i386_iso-hybrid/config/packages.chroot/
-cp live-tools_*.deb ${LIVEDEVELOP_GIT}/i386_iso-hybrid/config/packages.chroot/
-cp live-boot{_,-initramfs-tools,-doc}*.deb  ${LIVEDEVELOP_GIT}/i386_iso-hybrid/config/packages.chroot/
-cp live-config{_,-sysvinit,-doc}*.deb  ${LIVEDEVELOP_GIT}/i386_iso-hybrid/config/packages.chroot/
+cp live-build_*.deb ${LIVEDEVELOP_GIT}/testing/i386_iso-hybrid/config/packages.chroot/
+cp live-tools_*.deb ${LIVEDEVELOP_GIT}/testing/i386_iso-hybrid/config/packages.chroot/
+cp live-boot{_,-initramfs-tools,-doc}*.deb  ${LIVEDEVELOP_GIT}/testing/i386_iso-hybrid/config/packages.chroot/
+cp live-config{_,-sysvinit,-doc}*.deb  ${LIVEDEVELOP_GIT}/testing/i386_iso-hybrid/config/packages.chroot/
 
-#cp live-build_*.deb ${LIVEDEVELOP_GIT}/i386_iso-hybrid-backports/config/packages.chroot/
-cp live-tools_*.deb ${LIVEDEVELOP_GIT}/i386_iso-hybrid-backports/config/packages.chroot/
-cp live-boot{_,-initramfs-tools,-doc}*.deb  ${LIVEDEVELOP_GIT}/i386_iso-hybrid-backports/config/packages.chroot/
-cp live-config{_,-sysvinit,-doc}*.deb  ${LIVEDEVELOP_GIT}/i386_iso-hybrid-backports/config/packages.chroot/
-
-#cp live-build_*.deb ${LIVEDEVELOP_GIT}/amd64_iso-hybrid/config/packages.chroot/
-cp live-tools_*.deb ${LIVEDEVELOP_GIT}/amd64_iso-hybrid/config/packages.chroot/
-cp live-boot{_,-initramfs-tools,-doc}*.deb  ${LIVEDEVELOP_GIT}/amd64_iso-hybrid/config/packages.chroot/
-cp live-config{_,-sysvinit,-doc}*.deb  ${LIVEDEVELOP_GIT}/amd64_iso-hybrid/config/packages.chroot/
 rm -R -f -v /tmp/live
