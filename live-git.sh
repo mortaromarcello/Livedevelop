f1649b31f30ed5259c5f6eb6e6e47a52cc98df7f
mortaromarcello <mortaromarcello@gmail.com>
2014-01-28 06:14:02 +0100

    modificato:   live-git.sh

--- live-git.sh
+++ live-git.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 ### ha bisogno di python 3.3 (installarlo da debian testing) ###
 [[ ! -d /tmp/live ]] && mkdir -p /tmp/live; cd /tmp/live
-LIVEDEVELOP="/mnt/sda7/livedevelop/testing/i386_iso-hybrid"
-LIVEDEVELOP_GIT="/mnt/sda7/livedevelop-git/testing"
+LIVEDEVELOP="/mnt/sda7/livedevelop/jessie/i386_iso-hybrid"
+LIVEDEVELOP_GIT="/mnt/sda7/livedevelop-git/jessie"
 git clone git://live.debian.net/git/live-build.git
 git clone git://live.debian.net/git/live-boot.git
 git clone git://live.debian.net/git/live-config.git
-#git clone git://live-systems.org/git/live-manual.git
+git clone git://live-systems.org/git/live-manual.git
 git clone git://live-systems.org/git/live-tools.git
 cd live-build && dpkg-buildpackage -b -uc -us && cd ..
 cd live-boot && dpkg-buildpackage -b -uc -us && cd ..
 cd live-config && dpkg-buildpackage -b -uc -us && cd ..
-#cd live-manual && dpkg-buildpackage -b -uc -us && cd ..
+cd live-manual && dpkg-buildpackage -b -uc -us && cd ..
 cd live-tools && dpkg-buildpackage -b -uc -us && cd ..
 dpkg -i live-build_*.deb
 dpkg -i live-boot{_,-initramfs-tools,-doc}*.deb
